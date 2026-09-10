3f1b895f7eaa1c48b684f11c5093e40d6e8f57f3
shubhambyte-by <shubhamsahu1771@gmail.com>
2026-09-10 11:30:46 +0530

    variable

--- code.cpp
+++ code.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    int a = 23;
+    char x = 'a';
+    float PI = 3.14F;
+    cout<<typeid(PI).name()<<endl;
+    cout<<typeid(x).name()<<endl;
+    cout<<typeid(a).name()<<endl;
+    cout<<typeid(23).name()<<endl;
     return 0;
 }
