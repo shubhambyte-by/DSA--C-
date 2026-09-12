d46c0d6c5c10e49eb7e06206e904c28a39803e77
shubhambyte-by <shubhamsahu1771@gmail.com>
2026-09-12 23:54:40 +0530

    update

--- code.cpp
+++ code.cpp
@@ -1,13 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
-    int a = 23;
-    char x = 'a';
-    float PI = 3.14F;
-    cout<<typeid(PI).name()<<endl;
-    cout<<typeid(x).name()<<endl;
-    cout<<typeid(a).name()<<endl;
-    cout<<typeid(23).name()<<endl;
+int main()
+{
+    //Sum of odd number 
+    int i,n;
+    int sum;
+    sum = 0;
+    i = 0;
+    cout<<"Enter value of n :";
+    cin>>n;
+    while(i<=n)
+    {
+        if (i%2!=0){
+            sum = sum + i;
+        
+        }
+     i = i + 1;
+    }
+    cout<<sum;
     return 0;
 }
+git status
+git add .
+git commit -m "update"
+git push origin main
---
+++ operator.cpp
@@ -0,0 +1,85 @@
+// arithmetic operators in C++
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int age;
+    // cout<<"Enter your age :";
+    // cin>>age;
+    // if (age>=18)
+    // {
+    //     cout<<"You are eligible to vote.";
+    // }
+    // else
+    // {
+    //     cout<<"You are not eligible to vote.";
+    // }
+    // int num;
+    // cout << "Enter a number :";
+    // cin >> num;
+    // if (num % 2 == 0)
+    // {
+    //     cout << "The number is even.";
+    // }
+    // else
+    // {
+    //     cout << "The number is odd.";
+    // }
+
+    // if (num % 2)
+    // {
+    //     cout << "NUmber is even " << endl;
+    // }
+    // else
+    // {
+    //     cout << "Number is odd "
+    // }
+    // int marks;
+    // cout << "Enter marks : ";
+    // cin >> marks;
+    // if (marks <= 100 && marks > 90)
+    // {
+    //     cout << "A";
+    // }
+
+    // else if (marks <= 90 && marks > 80)
+    // {
+    //     cout << "B";
+    // }
+
+    // else if (marks <= 80 && marks > 70)
+    // {
+    //     cout << "C";
+    // }
+
+    // else if (marks <= 70 && marks > 60)
+    // {
+    //     cout << "D";
+    // }
+
+    // else if (marks <= 60 && marks > 50)
+    // {
+    //     cout << "E";
+    // }
+    // char ch;
+    // cout<<"Enter a charactor";
+    // cin >> ch;
+    // if (ch >= 'a' && ch <= 'z')
+    // {
+    //     cout << "Lowercase";
+    // }
+    // else if (ch >= 'A' && ch <= 'B')
+    // {
+    //     cout << "UPPERcase";
+    // }
+
+    int i,n
+    cout << "Enter value of n: ";
+    cin >> n;
+    cout << (n >= 0 ? "Positive" : "Negative ");
+    while(true)
+    {
+        cout<<i;
+    }
+    return 0;
+}
